24585aed26a229ec49f947dbf6664878bd62ce03
koscida <britkos@gmail.com>
2023-08-27 20:46:29 -0400

    add empty springboot project

---
+++ src/main/java/com/koscida/svdapi/svdapi/SvdApiApplication.java
@@ -0,0 +1,13 @@
+package com.koscida.svdapi.svdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koscida/svdapi/svdapi/SvdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koscida.svdapi.svdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
